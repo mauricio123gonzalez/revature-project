87d229829ff29432f18416c4146ac1bceb2438e4
mauricio123gonzalez <mauricio123gonzalez@gmail.com>
2021-01-11 09:22:06 -0600

    4

--- blue_bank/src/main/java/com/blue_bank/main/PlayerMain.java
+++ blue_bank/src/main/java/com/blue_bank/main/PlayerMain.java
@@ -1,9 +1,14 @@
 package com.blue_bank.main;
 
+import java.util.Scanner;
+
 public class PlayerMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int se=0;
+		//BlueBankSearch
 
 	}
 
